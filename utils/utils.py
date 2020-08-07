7126436a7bee7ff4928812ec8f5c917fb6066bf3
zwbgood6 <zwbgood6@qq.com>
2020-08-07 13:32:22 -0400

    separate the model and add two optimizers

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,33 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import os
+from matplotlib.lines import Line2D
+
+def plot_grad_flow(named_parameters, folder_name):
+    '''Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+    
+    Usage: Plug this function in Trainer class after loss.backwards() as 
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow
+    
+    Source: https://discuss.pytorch.org/t/check-gradient-flow-in-network/15063/10
+    '''
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)   
+    plt.savefig('./result/{}/plot/gradient_flow.png'.format(folder_name))
+    plt.close()
 
 def create_loss_list(loss_logger, kld=True):
     if loss_logger is None:
