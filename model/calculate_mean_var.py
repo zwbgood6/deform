21c1f0c5d38eb926582ecc62c29c3b24d8d38de4
zwenbo <zwbgood6@qq.com>
2021-04-18 21:23:01 -0700

    clean code

--- model/calculate_mean_var.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-action = np.load("/home/zwenbo/Documents/research/deform/plan_result/test_e2c/KL_action_1000.npy")
-state = np.load("/home/zwenbo/Documents/research/deform/plan_result/test_e2c/KL_image_1000.npy")
-
-print("action mean is:", action.mean())
-print("action std is:", np.sqrt(action.std()))
-print("state mean is:", state.mean())
-print("state std is:", np.sqrt(state.std()))
